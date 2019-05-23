e2bccb5dbc918865f3adb75fbd156436cd86a347
Ian Winstanley <ianwinstanley8@gmail.com>
2019-05-23 20:13:04 +0100

    Added some basic score prediction (about 12% accurate)

--- learner.py
+++ learner.py
@@ -26,8 +26,12 @@ raw_test_input = f.readlines()
 
 training_data = []
 training_labels = []
+home_goal_training_labels = []
+away_goal_training_labels = []
 test_data = []
 test_labels = []
+home_goal_test_labels = []
+away_goal_test_labels = []
 
 for line in raw_training_input:
     entry_list = ast.literal_eval(line)
@@ -35,6 +39,14 @@ for line in raw_training_input:
         entry = list(entry)
         training_data.append(entry[:18])
         training_labels.append(entry[18])
+        if entry[19] > 4:
+            home_goal_training_labels.append(4)
+        else:
+            home_goal_training_labels.append(entry[19])
+        if entry[20] > 4:
+            away_goal_training_labels.append(4)
+        else:
+            away_goal_training_labels.append(entry[20])
 
 for line in raw_test_input:
     entry_list = ast.literal_eval(line)
@@ -42,6 +54,14 @@ for line in raw_test_input:
         entry = list(entry)
         test_data.append(entry[:18])
         test_labels.append(entry[18])
+        if entry[19] > 4:
+            home_goal_test_labels.append(4)
+        else:
+            home_goal_test_labels.append(entry[19])
+        if entry[20] > 4:
+            away_goal_test_labels.append(4)
+        else:
+            away_goal_test_labels.append(entry[20])
 
 for count in range(len(training_data)):
     training_data[count] = np.asarray(training_data[count])
@@ -66,7 +86,6 @@ test_data = np.asarray(test_data)
 training_labels = np.asarray(training_labels)
 test_labels = np.asarray(test_labels)
 
-print(training_data)
 model = keras.Sequential([
   keras.layers.Dense(18, activation=tf.nn.selu),
   keras.layers.Dense(10, activation=tf.nn.tanh),
@@ -78,8 +97,24 @@ model.compile(optimizer='SGD',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_data, training_labels, epochs=5)
+model.fit(training_data, training_labels, epochs=10)
 
 test_loss,test_acc = model.evaluate(test_data, test_labels)
 
+
+model.fit(training_data, home_goal_training_labels, epochs=5)
+
+test_loss,home_test_acc = model.evaluate(test_data, home_goal_test_labels)
+
+
+model.fit(training_data, away_goal_training_labels, epochs=5)
+
+test_loss,away_test_acc = model.evaluate(test_data, away_goal_test_labels)
+
 print("Test accuracy: ", test_acc)
+
+print("Home goal test accuracy: ", home_test_acc)
+
+print("Away goal test accuracy: ", away_test_acc)
+
+print("Approx correct score accuracy: ", home_test_acc * away_test_acc)
